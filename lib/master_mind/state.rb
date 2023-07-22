8b7ef42728ae53231b2ab996f28e3ad40621f6e3
Nick-Damico <nickalan82@icloud.com>
2023-07-21 22:20:29 -0400

    WIP: Add Class to manage game state

--- lib/master_mind/state.rb
+++ lib/master_mind/state.rb
@@ -4,10 +4,29 @@ module MasterMind
   class State
     PHASES = %i[start playing game_over].freeze
 
-    attr_reader :phase
+    attr_accessor :phase
 
     def initialize
       @phase = :playing
     end
+
+    def start?
+      phase == :start
+    end
+
+    def playing?
+      phase == :playing
+    end
+
+    def game_over?
+      phase == :game_over
+    end
+
+    def update(state)
+      raise ArgumentError, "Invalid state" unless PHASES.include?(state)
+
+      self.phase = state
+    end
+
   end
 end
--- spec/master_mind/state_spec.rb
+++ spec/master_mind/state_spec.rb
@@ -12,5 +12,35 @@ module MasterMind
       end
       expect(described_class::PHASES.length).to eq 3
     end
+
+    describe "#update" do
+      it "updates the state" do
+        expected_state = :game_over
+        subject.update(expected_state)
+
+        expect(subject.phase).to eq expected_state
+      end
+    end
+
+    describe "#playing?" do
+      it "returns true if the game is in the playing state" do
+        subject.update(:playing)
+        expect(subject.playing?).to eq true
+      end
+    end
+
+    describe "#start?" do
+      it "returns true if the game is in the start state" do
+        subject.update(:start)
+        expect(subject.start?).to eq true
+      end
+    end
+
+    describe "#game_over?" do
+      it "returns true if the game is in the game_over state" do
+        subject.update(:game_over)
+        expect(subject.game_over?).to eq true
+      end
+    end
   end
 end
